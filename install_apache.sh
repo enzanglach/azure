46119df55e15f0dd5debec2277a76278641b7f19
enzanglach <madperro@gmail.com>
2020-05-18 18:43:56 +0200

    Create install_apache.sh

---
+++ install_apache.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt-get -y update
+apt-get -y upgrade
+apt-get -y autoremove
+
+# install Apache2
+apt-get -y install apache2
+# write some HTML
+mv /var/www/html/index.html /var/www/html/index-orig.html
+h=$(hostname)
+d=$(date)
+echo \<center\>\<h1\>My Demo App on $hp at $d\</h1\>\<br/\>\</center\> > /var/www/html/index.html
